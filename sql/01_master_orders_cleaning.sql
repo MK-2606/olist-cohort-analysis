969113dc859a516f6caab4579a85e032b3563e63
MK-2606 <mansi.23b0311051@abes.ac.in>
2026-05-17 15:32:58 +0530

    Update 01_master_orders_cleaning.sql

--- sql/01_master_orders_cleaning.sql
+++ sql/01_master_orders_cleaning.sql
@@ -1,1 +1,23 @@
+SELECT
+  o.order_id,
+  c.customer_unique_id,
+  c.customer_city,
+  c.customer_state,
+  DATE(o.order_purchase_timestamp) AS order_date,
+  FORMAT_DATE('%Y-%m', o.order_purchase_timestamp) AS order_month,
+  o.order_status,
+  ROUND(SUM(p.payment_value), 2) AS order_revenue
 
+FROM `olist-cohort-analysis.olist_raw.Orders` AS o
+
+JOIN `olist-cohort-analysis.olist_raw.customers` AS c
+  ON o.customer_id = c.customer_id
+
+JOIN `olist-cohort-analysis.olist_raw.payments` AS p
+  ON o.order_id = p.order_id
+
+WHERE
+  o.order_status = 'delivered'
+  AND o.order_purchase_timestamp IS NOT NULL
+
+GROUP BY 1, 2, 3, 4, 5, 6, 7;
