e6b759eea177bc6aaffca1431b28787c0de94696
MK-2606 <mansi.23b0311051@abes.ac.in>
2026-05-17 15:31:50 +0530

    Update 02_cohort_base.sql

--- sql/02_cohort_base.sql
+++ sql/02_cohort_base.sql
@@ -1,1 +1,10 @@
+SELECT
+  customer_unique_id,
+  MIN(order_month) AS cohort_month,
+  MIN(order_date)  AS first_order_date,
+  COUNT(DISTINCT order_id)   AS total_orders,
+  ROUND(SUM(order_revenue), 2) AS total_spend
 
+FROM `olist-cohort-analysis.olist_analysis.master_orders`
+
+GROUP BY customer_unique_id;
